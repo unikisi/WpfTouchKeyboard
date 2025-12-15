11a01a1eda913221a02e5682fdb8b645107b8366
18640971008 <lucifering@126.com>
2025-12-15 13:40:48 +0800

    Add usercontrol test.

---
+++ WpfTouchKeyboard/WpfTouchKeyboard.Test/TestUserControl.xaml.cs
@@ -0,0 +1,16 @@
+using System.Windows.Controls;
+
+namespace WpfTouchKeyboard.Test
+{
+    /// <summary>
+    /// TestUserControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class TestUserControl : UserControl
+    {
+        public TestUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
